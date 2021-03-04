472a01bc1aee540ed86084deaa18af3e3558a1fe
yaNimotsi <yanimotsi@yanmdex.ru>
2021-03-04 12:14:44 +0300

    Add project files.

---
+++ AlgoritmsLesson4Task1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace AlgoritmsLesson4Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+
+    public class Banchmark
+    {
+        string[] arrStr = new string[10000];
+        string strToFind;
+        int indexStrToFind;
+
+        HashSet<string> hashStrings = new HashSet<string>();
+        public Banchmark()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                arrStr[i] = i.ToString();
+                hashStrings.Add(i.ToString());
+            }
+
+            GetStrToFind();
+        }
+
+        public void GetStrToFind()
+        {
+            Random random = new Random();
+
+            indexStrToFind = random.Next(arrStr.Length);
+            strToFind = arrStr[indexStrToFind];
+        }
+
+        public bool FindStrInHashSet()
+        {
+            return hashStrings.Contains(strToFind);
+        }
+
+        public string FindStrInArr()
+        {
+            foreach(var str in arrStr)
+            {
+                if (str == strToFind) return str;
+            }
+
+            return null;
+        }
+
+        [Benchmark]
+        public void TestFindInArr()
+        {
+            FindStrInArr();
+        }
+        [Benchmark]
+        public void TestFindInHashSet()
+        {
+            FindStrInHashSet();
+        }
+    }
+}
