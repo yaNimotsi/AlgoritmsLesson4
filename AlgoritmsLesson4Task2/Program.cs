88b5491d6ef2ce41a05b26a031aacd1b09c39763
Istomin.I.A <Istomin.I.A@Z4-ISTOMINIA>
2021-03-09 13:56:40 +0300

    update method remove. add code to update root node

--- AlgoritmsLesson4Task2/Program.cs
+++ AlgoritmsLesson4Task2/Program.cs
@@ -7,12 +7,26 @@ namespace AlgoritmsLesson4Task2
         static void Main(string[] args)
         {
             BinaryTree binaryTree = new BinaryTree();
-            binaryTree.AddItem(10);
+            binaryTree.AddItem(30);
             binaryTree.AddItem(15);
-            binaryTree.AddItem(9);
-            binaryTree.AddItem(8);
+            binaryTree.AddItem(45);
+            binaryTree.AddItem(10);
+            binaryTree.AddItem(18);
+            binaryTree.AddItem(40);
+            binaryTree.AddItem(60);
+            binaryTree.AddItem(5);
+            binaryTree.AddItem(13);
+            binaryTree.AddItem(17);
+            binaryTree.AddItem(19);
+            binaryTree.AddItem(35);
+            binaryTree.AddItem(43);
+            binaryTree.AddItem(50);
+            binaryTree.AddItem(75);
+
 
             TreeNode treeNode = binaryTree.GetNodeByValue(3);
+
+            binaryTree.RemoveItem(30);
         }
     }
 }
