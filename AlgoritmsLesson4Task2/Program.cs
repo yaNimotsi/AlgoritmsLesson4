3c0f1581f61756db4536c908b11cf7982598be51
IvanLiaz <nimotsiky@gmail.com>
2021-05-03 20:24:30 +0300

    Add project for Task2

---
+++ AlgoritmsLesson4Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoritmsLesson4Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
